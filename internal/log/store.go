5d1004c9f57b44b375e48243314973201c7accd6
michael abbott <dev@DESKTOP-PVIG02S>
2023-03-26 20:56:04 +0100

    store tests

--- internal/log/store.go
+++ internal/log/store.go
@@ -23,7 +23,7 @@ type store struct {
   size uint64
 }
 
-func (s *store) append(p []byte) (n uint64, pos uint64, err error) {
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
   s.mu.Lock()
   defer s.mu.Unlock()
   
@@ -36,13 +36,50 @@ func (s *store) append(p []byte) (n uint64, pos uint64, err error) {
   w, err := s.buf.Write(p)
 
   if err != nil {
-    return 0, 0, fmt.Errorf("failed to write buffer in store.append with error %w", err)
+    return 0, 0, fmt.Errorf("failed to write buffer in store.Append with error %w", err)
   }
 
   w += lenWidth
 
   s.size += uint64(w)
   return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+  s.mu.Lock()
+  defer s.mu.Unlock()
+
+  if err := s.buf.Flush(); err != nil {
+    return nil, fmt.Errorf("failed to flush buffer in store.Read with error %w", err)
+  }
+  size := make([]byte, lenWidth)
+  if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+    return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos), err)
+  }
+  b := make([]byte, enc.Uint64(size))
+  if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+    return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos+lenWidth), err)
+  }
+  return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+  s.mu.Lock()
+  defer s.mu.Unlock()
+  if err := s.buf.Flush(); err != nil {
+    return 0, fmt.Errorf("failed to flush write buffer in store.ReadAt with error %w", err)
+  }
+  return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+  s.mu.Lock()
+  defer s.mu.Unlock()
+  err := s.buf.Flush()
+  if err != nil {
+    return fmt.Errorf("failed to flush write buffer in store.Close with error %w", err)
+  }
+  return s.File.Close()
 }
 
 
