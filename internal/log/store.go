b50fe5bea3ae8a3c9275d815d29e79b05267fb9b
michael abbott <dev@DESKTOP-PVIG02S>
2023-03-26 21:39:03 +0100

    working on index file for segments

--- internal/log/store.go
+++ internal/log/store.go
@@ -9,89 +9,88 @@ import (
 )
 
 var (
-  enc = binary.BigEndian
+	enc = binary.BigEndian
 )
 
 const (
-  lenWidth = 8
+	lenWidth = 8
 )
 
 type store struct {
-  *os.File
-  mu sync.Mutex
-  buf *bufio.Writer
-  size uint64
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
 }
 
 func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
-  
-  pos = s.size
-  
-  if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
-    return 0, 0, fmt.Errorf("failed to binary write in store.append with error %w", err)
-  }
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-  w, err := s.buf.Write(p)
+	pos = s.size
 
-  if err != nil {
-    return 0, 0, fmt.Errorf("failed to write buffer in store.Append with error %w", err)
-  }
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, fmt.Errorf("failed to binary write in store.append with error %w", err)
+	}
 
-  w += lenWidth
+	w, err := s.buf.Write(p)
 
-  s.size += uint64(w)
-  return uint64(w), pos, nil
+	if err != nil {
+		return 0, 0, fmt.Errorf("failed to write buffer in store.Append with error %w", err)
+	}
+
+	w += lenWidth
+
+	s.size += uint64(w)
+	return uint64(w), pos, nil
 }
 
 func (s *store) Read(pos uint64) ([]byte, error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-  if err := s.buf.Flush(); err != nil {
-    return nil, fmt.Errorf("failed to flush buffer in store.Read with error %w", err)
-  }
-  size := make([]byte, lenWidth)
-  if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
-    return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos), err)
-  }
-  b := make([]byte, enc.Uint64(size))
-  if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
-    return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos+lenWidth), err)
-  }
-  return b, nil
+	if err := s.buf.Flush(); err != nil {
+		return nil, fmt.Errorf("failed to flush buffer in store.Read with error %w", err)
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos), err)
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, fmt.Errorf("failed to read file at position %d in store.Read with error %w", int64(pos+lenWidth), err)
+	}
+	return b, nil
 }
 
 func (s *store) ReadAt(p []byte, off int64) (int, error) {
-  s.mu.Lock()
-  defer s.mu.Unlock()
-  if err := s.buf.Flush(); err != nil {
-    return 0, fmt.Errorf("failed to flush write buffer in store.ReadAt with error %w", err)
-  }
-  return s.File.ReadAt(p, off)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, fmt.Errorf("failed to flush write buffer in store.ReadAt with error %w", err)
+	}
+	return s.File.ReadAt(p, off)
 }
 
 func (s *store) Close() error {
-  s.mu.Lock()
-  defer s.mu.Unlock()
-  err := s.buf.Flush()
-  if err != nil {
-    return fmt.Errorf("failed to flush write buffer in store.Close with error %w", err)
-  }
-  return s.File.Close()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return fmt.Errorf("failed to flush write buffer in store.Close with error %w", err)
+	}
+	return s.File.Close()
 }
 
-
 func newStore(f *os.File) (*store, error) {
-  fi, err := os.Stat(f.Name())
-  if err != nil {
-    return nil, fmt.Errorf("failed to get stats for %s in newStore with error %w", f.Name(), err)
-  }
-  size := uint64(fi.Size())
-  return &store{
-    File: f,
-    size: size,
-    buf: bufio.NewWriter(f),
-  }, nil
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get stats for %s in newStore with error %w", f.Name(), err)
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
 }
--- internal/server/log.go
+++ internal/server/log.go
@@ -6,34 +6,34 @@ import (
 )
 
 type Log struct {
-  mu sync.Mutex
-  records []Record
+	mu      sync.Mutex
+	records []Record
 }
 
 type Record struct {
-  Value []byte `json:"value"`
-  Offset uint64 `json:"offset"`
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
 }
 
 var ErrOffSetNotFound = errors.New("offset not found")
 
 func (l *Log) Append(r Record) (uint64, error) {
-  l.mu.Lock()
-  defer l.mu.Unlock()
-  r.Offset = uint64(len(l.records))
-  l.records = append(l.records, r)
-  return r.Offset, nil
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	r.Offset = uint64(len(l.records))
+	l.records = append(l.records, r)
+	return r.Offset, nil
 }
 
 func (l *Log) Read(offset uint64) (Record, error) {
-  l.mu.Lock()
-  defer l.mu.Unlock()
-  if offset >= uint64(len(l.records)) {
-    return Record{}, ErrOffSetNotFound
-  }
-  return l.records[offset], nil
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if offset >= uint64(len(l.records)) {
+		return Record{}, ErrOffSetNotFound
+	}
+	return l.records[offset], nil
 }
 
 func NewLog() *Log {
-  return &Log{}
+	return &Log{}
 }
